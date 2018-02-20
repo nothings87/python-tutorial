ed5dc77ff987bf024c2f878b8f5fa2f40a900d6c
Jaegwang Lee <nothings87@gmail.com>
2018-02-20 04:59:36 +0000

    changed argument checking statement to check name and addr

--- cgi/server/cgi-bin/get.py
+++ cgi/server/cgi-bin/get.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
-print ("Content-type: text/html")
-print ()
-print ("<title>Test CGI</title>")
-print ("<p>Hello World!</p>")
+import cgi
+import cgitb
+cgitb.enable(display=0, logdir="./log")
+
+
+form = cgi.FieldStorage()
+
+if "name" not in form or "addr" not in form:
+	print("<H1>Error</H1>");
+	print("Please fill in the name and addr fields.");
+	return;
+
+print("name:", form["name"].value);
+print("addr:", form["addr"].value);
+
 
