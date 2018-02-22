3ddd16182b40a56ab508996b6a4a844e61aebfd1
Jaegwang Lee <nothings87@gmail.com>
2018-02-23 08:27:17 +0900

    Changed URI to DVRS/xxx. Changed sample JSON data.

--- http/client/http_post_json_req.py
+++ http/client/http_post_json_req.py
@@ -2,14 +2,41 @@
 
 import requests, json
 
-URL = 'http://localhost:8000/cgi-bin/post_with_json.py'
+URL = 'http://localhost:3000/DVRS/post-json.cgi'
 
-data = {'name': 'Jaegwang Lee',
-	'number': 'seven',
-	'job': 'developer'};
+sample = {
+    "resultCode" : 200,
+    "resultMessage" : "success",
+    "filelist" : [ 
+        { "video1" : "check" }, 
+        { "video2" : "no" } 
+    ],
+    "morelist" : [
+        {
+            "type" : "dir",
+            "name" : "Video",
+            "mimeType" : "",
+            "fileLength" : 0,
+            "playType" : 0
+        },
+        {
+            "type" : "file",
+            "name" : "video1.mp4",
+            "mimeType" : "video/mp4",
+            "fileLength" : 12000,
+            "playTime" : 240
+        }
+    ]
+}
 
-# data=json.dumps(data)
-response = requests.post(URL, data=json.dumps(data));
+
+sample_json_data = json.dumps(sample);
+
+# sample_json_data = json.load(open('sample01.json'))
+
+# Generate JSON from python data structure
+# response = requests.post(URL, data=json.dumps(sample));
+response = requests.post(URL, data=sample_json_data);
 
 print(">>>status code");
 print(response.status_code);
